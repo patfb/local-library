9834eaf34fcbce906ace07d20172707258a1bb5e
Patrick <patrick.f.boyne@gmail.com>
2018-03-31 00:54:22 -0400

    skeleton website created

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('users respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('You\'re so cool');
+});
 module.exports = router;
