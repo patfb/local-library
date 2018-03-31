7dd52cad4490338a96fd72514b4f7050b32e2c75
Patrick <patrick.f.boyne@gmail.com>
2018-03-31 16:34:43 -0400

    basic routing set up

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let favicon = require('serve-favicon');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 let app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
